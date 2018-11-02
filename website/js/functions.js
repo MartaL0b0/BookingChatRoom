d60c159924ae20e56a437736824dec19aa9d00f2
DESKTOP-4TPE2CT\MartaL0b0 <martaldp@gmail.com>
2018-11-02 10:21:54 +0000

    add logic for toggling between both tools

--- website/js/functions.js
+++ website/js/functions.js
@@ -7,3 +7,25 @@ function tableBooking(td) {
         td.style.backgroundColor = "#FFFFFF";
     }
 }
+
+const hide = (container) => {
+    
+    const actionContainer = document.querySelector(`#${container}`);
+    actionContainer.classList.remove("show");
+
+}
+
+function toggle(current) {
+
+    var other = (current == 'RoomChart') ? 'ChatBot' : 'RoomChart';
+    var div1 = document.getElementById(current);
+    var div2 = document.getElementById(other);
+  
+    if (div1.style.display == 'block'){
+        div1.style.display = 'none';
+    } else {
+        div1.style.display = 'block';
+        div2.style.display = 'none';
+    }
+
+}
